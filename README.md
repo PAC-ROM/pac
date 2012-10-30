950eb72c31104f7ba674c7c7fc8a3f9a4a258da7
szl.kiev <szl.kiev@gmail.com>
2012-10-30 11:55:04 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+PAC-man
+===============
+
+Getting started
+---------------
+First you must initialize a repository with our sources:
+
+    repo init -u git://github.com/PAC-man/android.git -b jellybean
+
+Then sync it up (This will take a while, so get a cup of coffee and some snickers):
+
+    repo sync
+
+
+Building P.A.C
+------------------------
+
+For building P.A.C you must cd to the working directory.
+Make sure you have your device tree sources, located on
+
+    cd device/-manufacturer-/-device-
+
+Now you can run our build script:
+
+    ./build-pac.sh -device-
+
+
+You can also use a second parameter for syncing sources before building
+
+    ./build-pac.sh -device- true
+
+
+There are also a few parameters that you can use together with before mentioned:
+
+* threads: Allows to choose a number of threads for syncing operation
+* clean: Removes intermediates and output files
+
+The usage is the same
+    
+    ./build-pac.sh -device- -parameters- true
+
+
+Parameters will be considered false unless you set them to true
+
+This will make a signed zip located on out/target/product/-device-.
+
+
---
+++ build.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# get current path
+reldir=`dirname $0`
+cd $reldir
+DIR=`pwd`
+
+# Colorize and add text parameters
+red=$(tput setaf 1)             #  red
+grn=$(tput setaf 2)             #  green
+cya=$(tput setaf 6)             #  cyan
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) #  red
+bldgrn=${txtbld}$(tput setaf 2) #  green
+bldylw=${txtbld}$(tput setaf 3) #  yellow
+bldblu=${txtbld}$(tput setaf 4) #  blue
+bldppl=${txtbld}$(tput setaf 5) #  purple
+bldcya=${txtbld}$(tput setaf 6) #  cyan
+txtrst=$(tput sgr0)             # Reset
+
+THREADS="16"
+DEVICE="$1"
+EXTRAS="$2"
+
+# get current version
+MAJOR=$(cat $DIR/vendor/pac/config/pac_common.mk | grep 'PAC_VERSION_MAJOR = *' | sed  's/PAC_VERSION_MAJOR = //g')
+MINOR=$(cat $DIR/vendor/pac/config/pac_common.mk | grep 'PAC_VERSION_MINOR = *' | sed  's/PAC_VERSION_MINOR = //g')
+MAINTENANCE=$(cat $DIR/vendor/pac/config/pac_common.mk | grep 'PAC_VERSION_MAINTENANCE = *' | sed  's/PAC_VERSION_MAINTENANCE = //g')
+VERSION=$MAJOR.$MINOR.$MAINTENANCE
+
+# if we have not extras, reduce parameter index by 1
+if [ "$EXTRAS" == "true" ] || [ "$EXTRAS" == "false" ]
+then
+   SYNC="$2"
+   UPLOAD="$3"
+else
+   SYNC="$3"
+   UPLOAD="$4"
+fi
+
+# get time of startup
+res1=$(date +%s.%N)
+
+# we don't allow scrollback buffer
+echo -e '\0033\0143'
+clear
+
+echo -e "${cya}Building ${bldgrn}P ${bldppl}A ${bldblu}C ${bldylw}v$VERSION ${txtrst}";
+
+echo -e "${cya}"
+./vendor/pac/tools/getdevicetree.py $DEVICE
+echo -e "${txtrst}"
+
+# decide what command to execute
+case "$EXTRAS" in
+   threads)
+       echo -e "${bldblu}Please write desired threads followed by [ENTER] ${txtrst}"
+       read threads
+       THREADS=$threads;;
+   clean)
+       echo -e ""
+       echo -e "${bldblu}Cleaning intermediates and output files ${txtrst}"
+       make clean > /dev/null;;
+esac
+
+# download prebuilt files
+echo -e ""
+echo -e "${bldblu}Downloading prebuilts ${txtrst}"
+cd vendor/cm
+./get-prebuilts
+cd ./../..
+
+# sync with latest sources
+echo -e ""
+if [ "$SYNC" == "true" ]
+then
+   echo -e "${bldblu}Fetching latest sources ${txtrst}"
+   repo sync -j"$THREADS"
+   echo -e ""
+fi
+
+rm -f out/target/product/*/obj/KERNEL_OBJ/.version
+
+# setup environment
+echo -e "${bldblu}Setting up environment ${txtrst}"
+. build/envsetup.sh
+
+# lunch device
+echo -e ""
+echo -e "${bldblu}Lunching device ${txtrst}"
+lunch "pac_$DEVICE-userdebug";
+
+echo -e ""
+echo -e "${bldblu}Starting compilation ${txtrst}"
+
+# start compilation
+brunch "pac_$DEVICE-userdebug";
+echo -e ""
+
+./vendor/pac/tools/squisher
+
+rm -f out/target/product/*/pac_*-ota-eng.*.zip
+
+# finished? get elapsed time
+res2=$(date +%s.%N)
+echo "${bldgrn}Total time elapsed: ${txtrst}${grn}$(echo "($res2 - $res1) / 60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds) ${txtrst}"
